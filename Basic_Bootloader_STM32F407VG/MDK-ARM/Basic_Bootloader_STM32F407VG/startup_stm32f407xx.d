1d7e147adc2bf2b10836382f285d27beace393cc
Abdullah Elnemr <alnmrbdallh328@gmail.com>
2024-08-07 00:34:43 +0300

    Added the bootloader & the application

---
+++ Basic_Bootloader_STM32F407VG/MDK-ARM/Basic_Bootloader_STM32F407VG/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+basic_bootloader_stm32f407vg\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ Basic_Bootloader_STM32F407VG/MDK-ARM/RTE/_Basic_Bootloader_STM32F407VG/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Basic_Bootloader_STM32F407VG' 
+ * Target:  'Basic_Bootloader_STM32F407VG' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
